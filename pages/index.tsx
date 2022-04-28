988a9e83ebf6d090143b19bc38070efcc64bbffd
sunss <sss@dataqin.com>
2022-04-28 16:21:06 +0800

    💄 地址调整 + 显示优化

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,8 @@ const Home: NextPage<HomeIndexProps> = ({ user }) => {
         {/*  */}
         <div className="mx-auto mt-6 max-w-4xl max-h-40 px-6 rounded-xl shadow-lg text-gray-500 bg-orange-200 py-3">
           <p>本站为我的个人网站重构版</p>
+          <p>技术栈将主要使用 next + tailwindcss</p>
+          <p>当前处于施工阶段中...，更多idea呈现，敬请期待~</p>
           <p>
             这里提供快速预览和了解我的相关介绍、技术文章、生活记录、最新文章与相关动态的汇总，作为一级入口使用
           </p>
@@ -42,11 +44,20 @@ const Home: NextPage<HomeIndexProps> = ({ user }) => {
           </div>
           <div className="min-h-full space-y-2">
             <div className="text-xl font-medium text-black">小帅の技术博客</div>
+            <p className="text-gray-500">
+              备用地址：
+              <a
+                className="text-gray-500"
+                target="_blank"
+                href="https://js-banana.github.io/blog"
+                rel="noreferrer"
+              ></a>
+            </p>
             <p>
               <a
                 target="_blank"
                 className="font-normal text-md text-green-600"
-                href="https://serverless-page-bucket-ybyz8f6i-1258454899.cos-website.ap-hongkong.myqcloud.com"
+                href="https://ssscode.com"
                 rel="noreferrer"
               >
                 ✅ 点击这里，访问我的技术博客
